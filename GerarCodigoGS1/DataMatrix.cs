a04337ee143d729d718e1bb04ded09eaf8fe4ade
Maycon Wisley <maycon.wisley@outlook.com>
2025-11-27 14:07:36 -0300

    Ajuste

--- GerarCodigoGS1/DataMatrix.cs
+++ GerarCodigoGS1/DataMatrix.cs
@@ -1,5 +1,7 @@
 ﻿using SkiaSharp;
 using ZXing;
+using ZXing.Datamatrix;
+using ZXing.Datamatrix.Encoder;
 using ZXing.SkiaSharp;
 
 namespace GerarCodigoGS1;
@@ -8,18 +10,21 @@ public static class DataMatrix
 {
     public static void GerarDataMatrix(string content, string outputPath, int size = 300)
     {
+        var gs1Content = $"{content}";
         var write = new BarcodeWriter
         {
             Format = BarcodeFormat.DATA_MATRIX,
-            Options = new ZXing.Common.EncodingOptions
+            Options = new DatamatrixEncodingOptions
             {
                 Width = size,
                 Height = size,
-                Margin = 1
+                Margin = 1,
+                SymbolShape = SymbolShapeHint.FORCE_SQUARE,
+                GS1Format = true
             }
         };
 
-        var bitmap = write.Write(content);
+        var bitmap = write.Write(gs1Content);
         using var image = SKImage.FromBitmap(bitmap);
         using var data = image.Encode(SKEncodedImageFormat.Png, 100);
         using var stream = File.OpenWrite(outputPath);
